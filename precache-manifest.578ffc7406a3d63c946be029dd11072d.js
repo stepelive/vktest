1bc674b3959806cc03bf67b7c422850462cfd505
Kirill Stepanov <iostepanovk@gmail.com>
2019-05-17 14:19:44 +0500

    Updates

---
+++ precache-manifest.578ffc7406a3d63c946be029dd11072d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vktest/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "a43572ad29ab9921d6e0",
+    "url": "/vktest/static/js/runtime~main.a43572ad.js"
+  },
+  {
+    "revision": "67a5b69e89d0ce3ded12",
+    "url": "/vktest/static/js/main.67a5b69e.chunk.js"
+  },
+  {
+    "revision": "07a9343133782c4737da",
+    "url": "/vktest/static/js/2.07a93431.chunk.js"
+  },
+  {
+    "revision": "67a5b69e89d0ce3ded12",
+    "url": "/vktest/static/css/main.4273c5a4.chunk.css"
+  },
+  {
+    "revision": "07a9343133782c4737da",
+    "url": "/vktest/static/css/2.89b27a51.chunk.css"
+  },
+  {
+    "revision": "7e7ce3f9b025292ae8315381fa2af8bf",
+    "url": "/vktest/index.html"
+  }
+];
