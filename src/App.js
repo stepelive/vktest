d06e49fd4249bc6c0feae7e48578ec660265bd8f
Kirill Stepanov <iostepanovk@gmail.com>
2019-05-22 11:28:35 +0500

    Answers

--- src/App.js
+++ src/App.js
@@ -9,18 +9,66 @@ import Questions from './panels/Questions'
 class App extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			activePanel: 'surveys',
 			history: ['surveys'],
 			dataSurveys:undefined,
 			activeSurvey:undefined,
-			activeQuestion:undefined
+			activeQuestion:undefined,
+			answers:[]
 		};
 	}
 	componentDidMount() {
-		axios.get('https://web20190521031103.azurewebsites.net/').then((x)=>this.setState({dataSurveys: x.data}));
+		//axios.get('https://web20190521031103.azurewebsites.net/').then((x)=>this.setState({dataSurveys: x.data}));
 
+		this.setState({dataSurveys: [
+			{
+			  "title": "Тестовый опрос",
+			  "description": "Ну просто тестовый опрос,чо",
+			  "questions": [
+				{
+				  "position": 1,
+				  "price": 5.0,
+				  "title": "Вы кто?",
+				  "type": "checkbox",
+				  "avilableAnswers": [
+					{ "title": "Жопка", "questionId": 0, "question": null, "id": 0 },
+					{ "title": "Попка", "questionId": 0, "question": null, "id": 1 },
+					{ "title": "Да", "questionId": 0, "question": null, "id": 2 },
+					{ "title": "Нет", "questionId": 0, "question": null, "id": 3 }
+				  ],
+				  "id": 0
+				},
+				{
+					"position": 2,
+					"price": 5.0,
+					"title": "Вы что?",
+					"type": "checkbox",
+					"avilableAnswers": [
+					  { "title": "123123", "questionId": 1, "question": null, "id": 4 },
+					  { "title": "3213", "questionId": 1, "question": null, "id": 5},
+					  { "title": "12331", "questionId": 1, "question": null, "id": 6 },
+					  { "title": "1233", "questionId": 1, "question": null, "id": 7 }
+					],
+					"id": 1
+				  },
+				  {
+					"position": 3,
+					"price": 5.0,
+					"title": "Вы кто?",
+					"type": "checkbox",
+					"avilableAnswers": [
+					  { "title": "))", "questionId": 2, "question": null, "id": 8 },
+					  { "title": "((Попка))", "questionId": 2, "question": null, "id": 9 },
+					  { "title": "((Да))", "questionId": 2, "question": null, "id": 10 },
+					  { "title": "9((((", "questionId": 2, "question": null, "id": 11 }
+					],
+					"id": 2
+				  }
+			  ],
+			  "id": 0
+			}
+		  ]});
 		connect.subscribe((e) => {
 			switch (e.detail.type) {
 				case 'VKWebAppGetUserInfoResult':
@@ -38,13 +86,48 @@ class App extends React.Component {
 		var activeSurvey = this.state.dataSurveys.filter((s)=>{
 			return s.id == e.currentTarget.dataset.survey;
 		});
-		var activeQuestion = activeSurvey.questions[0];
+		var activeQuestion = activeSurvey[0].questions[0];
 		console.log(activeSurvey);
 
 		this.setState({activePanel: 'survey', activeQuestion:activeQuestion, activeSurvey:activeSurvey[0]})
 	}
+	setAnswer = (e) =>{
+		var answers = this.state.answers;
+		if(!e.answer){
+			var has = answers.find((eb)=>{return eb.id == e.id});
+			var index = answers.indexOf(has);
+			if(index == -1){
+				console.log('not has in index')
+			}
+			else{
+				answers.splice(index,1);
+			}
+		}
+		else{
+			answers.push(e);
+		}
+		this.setState({ answers: answers})
+		console.log(this.state.answers);
+	}
+	sendAnswers = () =>{
+		var model = {
+			survey: this.state.activeSurvey.id,
+			answers: this.state.answers
+		}
+		console.log(model);
+		this.nextQuestion();
+	}
+	nextQuestion = () =>{
+		var currentQuestionIndex = this.state.activeSurvey.questions.indexOf(this.state.activeQuestion);
+		var next = this.state.activeSurvey.questions[currentQuestionIndex+1];
+		if(next == undefined){
+			this.setState({ activePanel:'surveys' })
+		}
+		else{
 
-
+			this.setState({ activeQuestion: next})
+		}
+	}
 
 	go = (e) => {
 		this.setState({ activePanel: e.currentTarget.dataset.to })
@@ -54,7 +137,7 @@ class App extends React.Component {
 		return (
 			<View activePanel={this.state.activePanel}>
 				<Surveys  id="surveys" go={this.go} surveys={this.state.dataSurveys} go_survey={this.survey}   />
-				<Questions id="survey" go={this.go} activeQuestion={this.state.activeQuestion} activeSurvey={this.state.activeSurvey}></Questions>
+				<Questions id="survey" go={this.go} nextQuestion={this.nextQuestion} setAnswer={this.setAnswer} sendAnswer={this.sendAnswers} activeQuestion={this.state.activeQuestion} activeSurvey={this.state.activeSurvey}></Questions>
 			</View>
 		);
 	}
--- src/panels/Questions.js
+++ src/panels/Questions.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import {Panel,Header,  PanelHeader, HeaderButton, Group} from '@vkontakte/vkui';
+import {Panel,List,Button, Div,Checkbox, PanelHeader, HeaderButton, Group} from '@vkontakte/vkui';
 import Icon24BrowserBack from '@vkontakte/icons/dist/24/browser_back';
 
 
-const Questions = ({id, go, sendAnswer, activeQuestion ,activeSurvey}) => (
+function WriteAnswer(orig, e, sender){
+	var answer = {
+		answer:orig.target.checked,
+		id:e.id
+	};
+
+	sender(answer);
+}
+
+function RenderAnswers(props){
+	const answers = props.question.avilableAnswers;
+	const sender = props.sender;
+	var listItem = answers.map((e)=>
+		<Checkbox onChange={(orig) => WriteAnswer(orig,e,sender)} key={e.id.toString()}>{e.title}</Checkbox>
+	);
+	return <List>{listItem}</List>
+}
+
+const Questions = ({id, go, sendAnswer,setAnswer, activeQuestion ,activeSurvey}) => (
 	<Panel id={id}>
 		<PanelHeader
 			left={<HeaderButton onClick={go} data-to="surveys">
@@ -11,8 +29,11 @@ const Questions = ({id, go, sendAnswer, activeQuestion ,activeSurvey}) => (
 			</HeaderButton>}>            
 			{activeSurvey.title}
 		</PanelHeader>  
-        <Group>
-
+        <Group title={activeQuestion.title}>
+			<RenderAnswers sender={setAnswer} question={activeQuestion}></RenderAnswers>
+			<Div>
+       			<Button size="xl" onClick={sendAnswer} level="secondary">Дальше</Button>
+     		</Div>
       	</Group> 
 	</Panel>
 );
